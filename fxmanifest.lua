fb4af24a31dd9191229b6bb275ea592ad14a5379
Benito "DK_Den7e" L <benitoll1993@gmail.com>
2022-06-23 22:26:45 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,6 @@
 fx_version "cerulean"
+use_fxv2_oal 'yes'
+lua54        'yes'
 game { "gta5" }
 
 author 'snakewiz'
@@ -7,6 +9,20 @@ repository 'https://github.com/pedr0fontoura/fivem-appearance'
 version '1.3.0'
 
 client_script 'game/dist/index.js'
+
+client_scripts {
+	--'ox_esx_skin/client/constants.lua',
+	--'ox_esx_skin/client/utils.lua',
+	--'ox_esx_skin/client/customisation.lua',
+	--'ox_esx_skin/client/nui.lua',
+	'ox_esx_skin/client/main.lua',
+	'ox_esx_skin/client/outfits.lua',
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'ox_esx_skin/server/main.lua'
+}
 
 files {
   'web/dist/index.html',
